095c352f7389d7372df556c0ae32950d2e7abe74
juli <bricenojuliana@gmail.com>
2024-09-19 15:58:04 -0500

    calculadora

--- src/main/java/edu/escuelaing/arem/ASE/app/CalcreflexBEServer.java
+++ src/main/java/edu/escuelaing/arem/ASE/app/CalcreflexBEServer.java
@@ -5,13 +5,17 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class CalcreflexBEServer {
-    public static void main(String[] args) throws IOException, URISyntaxException {
+    public static void main(String[] args) throws IOException, URISyntaxException, InvocationTargetException, NoSuchMethodException, IllegalAccessException {
         ServerSocket serverSocket = null;
         try {
             serverSocket = new ServerSocket(36000);
@@ -52,10 +56,12 @@ public class CalcreflexBEServer {
             URI requri = getReqURI(firstLine);
 
             if (requri.getPath().startsWith("/compreflex")) {
+                String result = computeMathCommand(requri.getQuery());
                 outputLine = "HTTP/1.1 200 OK\r\n"
                         + "Content-Type: application/json\r\n"
                         + "\r\n"
-                        + "{\"name\":\"John\", \"age\":30, \"car\": null}";
+                        + "{\"result\":\"" + result + "\"}\n";
+
             } else {
                 outputLine = getDefaultResponse();
             }
@@ -88,5 +94,39 @@ public class CalcreflexBEServer {
     public static URI getReqURI(String firstLine) throws URISyntaxException {
         String rurl = firstLine.split(" ")[1];
         return new URI(rurl);
+    }
+
+    public static String computeMathCommand(String command) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        Pattern pattern = Pattern.compile("comando=(\\w+)\\(([^)]+)\\)");
+        Matcher matcher = pattern.matcher(command);
+
+        if (matcher.find()) {
+            String funcion = matcher.group(1);
+            String[] numeros = matcher.group(2).split(",");
+
+            System.out.println("Función: " + funcion);
+            System.out.println("Números:");
+            Class[] parameterTypes = new Class[numeros.length];
+            int i = 0;
+            Object[] params = new Object[numeros.length];
+            for (String num : numeros) {
+                System.out.println(num);
+                parameterTypes[i] = double.class;
+                params[i] = (double) Double.parseDouble(num);
+                i++;
+            }
+
+            Class c = Math.class;
+
+            Method m = c.getDeclaredMethod(funcion, parameterTypes);
+
+            String resp = m.invoke(null, params).toString();
+
+            return resp;
+        }
+
+
+
+        return "";
     }
 }
