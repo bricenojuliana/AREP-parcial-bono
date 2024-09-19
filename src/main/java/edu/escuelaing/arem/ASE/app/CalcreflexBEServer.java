a4f824ec26dcff3d2468ddeab2be5822171a48c4
juli <bricenojuliana@gmail.com>
2024-09-19 15:08:17 -0500

    fachada y calc con conexion, aun no hay calculos

---
+++ src/main/java/edu/escuelaing/arem/ASE/app/CalcreflexBEServer.java
@@ -0,0 +1,92 @@
+package edu.escuelaing.arem.ASE.app;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class CalcreflexBEServer {
+    public static void main(String[] args) throws IOException, URISyntaxException {
+        ServerSocket serverSocket = null;
+        try {
+            serverSocket = new ServerSocket(36000);
+        } catch (IOException e) {
+            System.err.println("Could not listen on port: 36000.");
+            System.exit(1);
+        }
+
+        boolean running = true;
+        while(running) {
+            Socket clientSocket = null;
+            try {
+                System.out.println("Listo para recibir ...");
+                clientSocket = serverSocket.accept();
+            } catch (IOException e) {
+                System.err.println("Accept failed.");
+                System.exit(1);
+            }
+
+            PrintWriter out = new PrintWriter(
+                    clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(clientSocket.getInputStream()));
+            String inputLine, outputLine;
+            boolean isfirstLine = true;
+            String firstLine = "";
+            while ((inputLine = in.readLine()) != null) {
+                System.out.println("Recibí: " + inputLine);
+                if (isfirstLine) {
+                    firstLine = inputLine;
+                    isfirstLine = false;
+                }
+                if (!in.ready()) {
+                    break;
+                }
+            }
+
+            URI requri = getReqURI(firstLine);
+
+            if (requri.getPath().startsWith("/compreflex")) {
+                outputLine = "HTTP/1.1 200 OK\r\n"
+                        + "Content-Type: application/json\r\n"
+                        + "\r\n"
+                        + "{\"name\":\"John\", \"age\":30, \"car\": null}";
+            } else {
+                outputLine = getDefaultResponse();
+            }
+
+            out.println(outputLine);
+            out.close();
+            in.close();
+            clientSocket.close();
+        }
+        serverSocket.close();
+    }
+
+    private static String getDefaultResponse() {
+        String response = "HTTP/1.1 200 OK\r\n"
+                + "Content-Type: text/html\r\n"
+                + "\r\n"
+                + "<!DOCTYPE html>\n"
+                + "<html>\n"
+                + "<head>\n"
+                + "<meta charset=\"UTF-8\">\n"
+                + "<title>Title of the document</title>\n"
+                + "</head>\n"
+                + "<body>\n"
+                + "<h1>Method Not Found</h1>\n"
+                + "</body>\n"
+                + "</html>\n";
+        return response;
+    }
+
+    public static URI getReqURI(String firstLine) throws URISyntaxException {
+        String rurl = firstLine.split(" ")[1];
+        return new URI(rurl);
+    }
+}
